5eb416da8acb52efa6e926491233b227ff50bb06
husain <husain@husain.(none)>
2013-07-06 07:12:13 -0700

    from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', '', function(request, response) {
+  var data = fs.readFileSync('index.html');
+  var buffer = new Buffer(data);
+  response.send(buffer.toString('utf-8'));
+  //response.send("Hello World!");
 });
 
 var port = process.env.PORT || 5000;
