b6d07f5cce96ef8e8fb1cede4824254964164f51
hgasimov <gasimov.huseyn@gmail.com>
2013-07-07 01:39:28 -0700

    Add grader.js and checks.json

--- grader.js
+++ grader.js
@@ -21,11 +21,37 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_TMP = "tmp.html";
+var URL_DEFAULT = "http://www.google.com";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var buildfn = function(html_file, checks) {
+    var save_result = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            //console.error("Wrote %s", html_file);
+            fs.writeFileSync(html_file, result);
+            grade(html_file, checks);
+        }
+    };
+    return save_result;
+};
+
+var downloadURL = function(url, checks, html_file) {
+    //console.log('Starting to download URL ' + url);
+    html_file = html_file || HTMLFILE_TMP;
+    var save_result = buildfn(html_file, checks);
+    //console.log('Finished to build a save_result function. Starting to get url ...');
+    rest.get(url).on('complete', save_result);
+    //console.log('Get url has been sent');
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +87,26 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var grade = function (file, checks) {
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    //console.log(program.url);
+    if (typeof program.url !== 'undefined') 
+        downloadURL(program.url, program.checks, HTMLFILE_TMP);
+    else 
+        grade(program.file, program.checks);
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
